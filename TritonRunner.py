6e0a73425da509ea22f1983fb1043e91ba656e7c
hdgreen14 <105872251+hdgreen14@users.noreply.github.com>
2023-09-18 09:13:59 -0400

    Update TritonRunner.py

--- TritonRunner.py
+++ TritonRunner.py
@@ -1,20 +1,59 @@
 import os
+import glob
+import re
+import csv
 from collections import OrderedDict
 import subprocess
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 
-"""
-A simple script that runs the triton performance analyzer a set number of times for a list of given commands,
-and organizes the output csv files by model and batchsize.
-"""
+def getValue(filename: str, yPara, linename):
+    """
+    Inputs: filename, paramater to search for
+    Output: List of values that match header asked for in yPara as a dictionary in form filename : [values of row]
+    """
 
-commands = OrderedDict()
-commands['deepmet'] = "perf_analyzer -m deepmet -u triton-5gb.cms.geddes.rcac.purdue.edu:8001 -i grpc --percentile=95 --async -p 30000 --concurrency-range 4:4 -b 100 --model-repository=/depot/cms/purdue-af/triton/models/ --input-data zero"
+    valuedict = {}
+    yval = np.array([])
+
+    data_dict = {}
+
+    with open(filename, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        #can't skip if a file onlly has headers - fix that
+        keys = next(csv_reader)   
+        columns = [[] for i in range(len(keys))] 
+        for row in csv_reader:
+            for i, value in enumerate(row):
+                if re.search( r'^\d+(\.\d+)?$', value): #ignore header
+                    columns[i].append(value)
+        
+        data_dict = {keys[i]: columns[i] for i in range(len(keys))}
+
+        if yPara in data_dict.keys():
+            value =  np.fromiter(data_dict[yPara], dtype=float)
+            if (yPara != "Concurrency") and (yPara != "Client Recv"):
+                value = np.divide(value, 1000)
+                yval = np.append(yval, values= value)
+            else:
+                yval = np.append(yval, values = value)
+        valuedict[linename] = yval
+        return valuedict.values()
+    
+
+
+
+commands = {}
+commands['deepmet'] = "perf_analyzer -m deepmet -u triton-5gb.cms.geddes.rcac.purdue.edu:8001 -i grpc --percentile=95 --async -p 10000 --concurrency-range 4:4 -b 100 --model-repository=/depot/cms/purdue-af/triton/models/ --input-data zero"
+#commands['particlenet'] = "perf_analyzer -m particlenet -u triton-5gb.cms.geddes.rcac.purdue.edu:8001 -i grpc --percentile=95 --async -p 10000 --concurrency-range 4:4 -b 100 --model-repository=/depot/cms/purdue-af/triton/models/ --input-data zero"
+
 #commands['deeptau_nosplit'] = "perf_analyzer -m deeptau_nosplit --percentile=95 -u localhost:8021 -i grpc --async -p 9000 --concurrency-range 4:4 -b"
+graphTitles = {}
+for model in commands.keys():
+    graphTitles[model] = {} #in form {model : {batch : {boxname : values} , .. ,  } }
 
-#define csv headers
-fileheaders = "\"Concurrency,Inferences/Second,Client Send,Network+Server Send/Recv,Server Queue,Server Compute Input,Server Compute Infer,Server Compute Output,Client Recv,p50 latency,p90 latency,p95 latency,p99 latency\""
-
-
+#singularity run /depot/cms/users/green642/tritoncli.sif
 outdir = os.getcwd() + "/output/"
 if not os.path.isdir(outdir):
     subprocess.run(f"mkdir {outdir}", shell=True)
@@ -22,16 +61,45 @@ for model, command in commands.items(): #adapted from SONIC model optimization s
     outdirmod = f"{outdir}{model}/" #current working directory
     if not os.path.isdir(outdirmod):
         subprocess.run(f"mkdir {outdirmod}", shell=True)
-    for batch in [8]: #run perfanalyzer command
+    for batch in [8, 16, 32]: #run perfanalyzer command
         currentfile = f"{outdirmod}{model}_batch{batch}.csv" #working file
         if not os.path.isdir(currentfile):
-            subprocess.run(f"touch {currentfile}; echo {fileheaders} {currentfile}", shell=True) #add headers
+            subprocess.run(f"touch {currentfile}; echo \"Concurrency,Inferences/Second,Client Send,Network+Server Send/Recv,Server Queue,Server Compute Input,Server Compute Infer,Server Compute Output,Client Recv,p50 latency,p90 latency,p95 latency,p99 latency\" {currentfile}", shell=True) #headers
         tmp = command
         if not os.path.isdir(outdir):
             subprocess.run(f"touch {outdir}tempout.csv", shell=True)
         tmp += f" {batch} -f {outdir}tempout.csv; sed -n '2p' \"{outdir}tempout.csv\" >> \"{currentfile}\" "
 
-        print(tmp)
-        for repeat in range(0, 4): #repeat n times
+        for repeat in range(0, 3):
             subprocess.run(f"{tmp}", shell=True) #run command
+        #print(getValue(currentfile, "Inferences/Second", f"{model}, Batchsize: {batch}"))
+        graphTitles[model][batch] = getValue(currentfile, "Inferences/Second", f"{model}, Batchsize: {batch}")
+
+
+
+#make plot
+finaldict= {}
+for model, batchsize in graphTitles.items():
+    print("\nmodel", model)
+    
+    for key in batchsize:
+        finaldict[f'{model}, {key}:']= list(graphTitles[model][key])
         
+    arr = [] #flatten
+    for i in finaldict.values():
+        arr.append(np.ravel(i))
+    plt.boxplot(arr, labels= finaldict.keys(), vert=False)    
+    plt.tight_layout()
+    plt.savefig("test.png")
+    plt.show()
+
+
+
+models = graphTitles.keys() #list of models
+vals = graphTitles.values() #dict in form box title : values
+
+#print(dict(vals).values()) # {batchsize : {boxname : values} }
+#print(dict(dict(vals).values()).keys())
+#box = plt.boxplot(graphTitles.keys().keys(), labels=graphTitles.keys().values(), vert=False)
+
+subprocess.run(f"rm {outdir}tempout.csv", shell=True)
